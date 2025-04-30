5ef66efda9efc4d09ff5fe9f41d3273006703a3b
irisemarccellu <fonseca197415@gmail.com>
2025-04-30 02:14:14 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
 document.addEventListener("DOMContentLoaded", function () {
   const buyButtons = document.querySelectorAll(".buy-now");
 
-  buyButtons.forEach(function (button) {
-    button.addEventListener("click", function () {
+  buyButtons.forEach(button => {
+    button.addEventListener("click", function (e) {
+      e.preventDefault();
       window.open("https://entrega.logzz.com.br/share-page/memz3doog/421", "_blank");
     });
   });
