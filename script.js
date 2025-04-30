4672955d71f5dcef58a8f992f6e0ddfd43d847c3
irisemarccellu <fonseca197415@gmail.com>
2025-04-30 00:21:22 -0300

    Update script.js

--- script.js
+++ script.js
@@ -25,3 +25,21 @@ window.addEventListener('DOMContentLoaded', () => {
       document.getElementById('produtos').innerHTML = '<p>Erro ao carregar produtos.</p>';
     });
 });
+fetch("landing.json")
+  .then(response => response.json())
+  .then(data => {
+    const content = document.getElementById("conteudo");
+    content.innerHTML = `
+      <h2>${data.titulo}</h2>
+      <p>${data.descricao}</p>
+      <h3>Benefícios:</h3>
+      <ul>
+        ${data.beneficios.map(b => `<li>${b}</li>`).join("")}
+      </ul>
+    `;
+  })
+  .catch(error => {
+    const content = document.getElementById("conteudo");
+    content.innerHTML = `<p style="color: red;">Erro ao carregar o JSON 😢</p>`;
+    console.error("Erro ao carregar o JSON:", error);
+  });
